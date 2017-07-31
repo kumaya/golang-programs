ead2e7ad3a9304a6427bddf4d2cdbe6fdef18796
Raj Chaudhuri <rajch@hotmail.com>
2017-07-31 07:44:04 +0000

    Modifications made in class

--- matrix/matrixCalculator.go
+++ matrix/matrixCalculator.go
@@ -1,40 +1,54 @@
 package main
 
 import "fmt"
-import "matrix/operations"
+import "github.com/kumaya/golang-programs/matrix/operations"
+import "math/rand"
 
+func populateMatrix(m [][]int) [][]int {
+	len_r := len(m)
+	for i := 0; i < len_r; i++ {
+		c := m[i]
+		len_c := len(c)
+		for j := 0; j < len_c; j++ {
+			m[i][j] = rand.Intn(3)
+		}
+	}
+	return m
+}
 
 func main() {
-	rawFirstMatrix := matrix.GenerateMatrix(3, 4)
-	rawSecondMatrix := matrix.GenerateMatrix(3, 4)
-	firstMatrix := matrix.PopulateMatrix(rawFirstMatrix)
-	secondMatrix := matrix.PopulateMatrix(rawSecondMatrix)
+	rawFirstMatrix := matrix.New(3, 3)
+	rawSecondMatrix := matrix.New(3, 3)
+	firstMatrix := populateMatrix(rawFirstMatrix)
+	secondMatrix := populateMatrix(rawSecondMatrix)
 	fmt.Println("First matrix: ", firstMatrix)
 	fmt.Println("Second matrix: ", secondMatrix)
 	fmt.Println("")
-	
+
 	fmt.Println("*****ADDITION*****")
 	addRes, isValid := matrix.Add(firstMatrix, secondMatrix)
 	fmt.Println("Matrix Addition is valid: ", isValid)
-	fmt.Println("Matrix Addition Result: ", addRes)
+	fmt.Println("Matrix Addition Result: ", matrix.String(addRes))
 	fmt.Println("")
-	
+
 	fmt.Println("*****SUBTRACTION*****")
 	subtractRes, isValid := matrix.Subtract(firstMatrix, secondMatrix)
 	fmt.Println("Matrix Subtraction is valid: ", isValid)
-	fmt.Println("Matrix Subtraction Result: ", subtractRes)
+	fmt.Println("Matrix Subtraction Result: ", matrix.String(subtractRes))
 	fmt.Println("")
 
-	fmt.Println("*****MULTIPLICATION*****")
-	rawFirstMatrix = matrix.GenerateMatrix(3, 4)
-	rawSecondMatrix = matrix.GenerateMatrix(3, 4)
-	firstMatrix = matrix.PopulateMatrix(rawFirstMatrix)
-	secondMatrix = matrix.PopulateMatrix(rawSecondMatrix)
-	multiplyRes, isValid := matrix.Multiply(firstMatrix, secondMatrix)
-	fmt.Println("First matrix: ", firstMatrix)
-	fmt.Println("Second matrix: ", secondMatrix)
-	fmt.Println("")
-	fmt.Println("Matrix Multiplication is valid: ", isValid)
-	fmt.Println("Matrix Multiplication Result: ", multiplyRes)
-	fmt.Println("")
+	/*
+		fmt.Println("*****MULTIPLICATION*****")
+		rawFirstMatrix = matrix.GenerateMatrix(3, 4)
+		rawSecondMatrix = matrix.GenerateMatrix(3, 4)
+		firstMatrix = populateMatrix(rawFirstMatrix)
+		secondMatrix = populateMatrix(rawSecondMatrix)
+		multiplyRes, isValid := matrix.Multiply(firstMatrix, secondMatrix)
+		fmt.Println("First matrix: ", firstMatrix)
+		fmt.Println("Second matrix: ", secondMatrix)
+		fmt.Println("")
+		fmt.Println("Matrix Multiplication is valid: ", isValid)
+		fmt.Println("Matrix Multiplication Result: ", multiplyRes)
+		fmt.Println("")
+	*/
 }
--- matrix/operations/matrix_operations.go
+++ matrix/operations/matrix_operations.go
@@ -1,15 +1,20 @@
 package matrix
 
-import "math/rand"
+import "fmt"
 
-func GenerateMatrix(r int, c int) ([][]int) {
+// Matrix is a slice of slice of int of a specified size
+type Matrix [][]int
+
+// New creates a new matrix with r rows and c columns
+func New(r int, c int) [][]int {
 	mat := make([][]int, r)
-	for i:=0; i<r; i++ {
+	for i := range mat {
 		mat[i] = make([]int, c)
 	}
 	return mat
 }
 
+/*
 func PopulateMatrix(m [][]int) ([][]int){
 	len_r := len(m)
 	for i:=0; i<len_r; i++ {
@@ -21,51 +26,74 @@ func PopulateMatrix(m [][]int) ([][]int){
 	}
 	return m
 }
+*/
 
-func Add(m1 [][]int, m2 [][]int) ([][]int, bool){
-	m1_len_r := len(m1)
-	m1_len_c := len(m1[0])
-	m2_len_r := len(m2)
-	m2_len_c := len(m2[0])
-	res := GenerateMatrix(m1_len_r, m1_len_c)
-	var valid bool
-	valid = true
-	// fmt.Println(m1_len_r, m2_len_r, m1_len_c, m2_len_c)
-	if m1_len_r != m2_len_r || m1_len_c != m2_len_c {
-		valid = false
-	}
-	if valid {
-		for i:=0; i<m1_len_r; i++ {
-			for j:=0; j<m1_len_c; j++ {
-				res[i][j] = m1[i][j] + m2[i][j]
-			}
-		}
-	}
-	return res, valid
+func isvalid(m1 [][]int, m2 [][]int) (valid bool) {
+	m1_len_r, m1_len_c, m2_len_r, m2_len_c := len(m1), len(m1[0]), len(m2), len(m2[0])
+	valid = (m1_len_r == m2_len_r && m1_len_c == m2_len_c)
+	return
 }
 
-func Subtract(m1 [][]int, m2 [][]int) ([][]int, bool){
-	m1_len_r := len(m1)
-	m1_len_c := len(m1[0])
-	m2_len_r := len(m2)
-	m2_len_c := len(m2[0])
-	res := GenerateMatrix(m1_len_r, m1_len_c)
-	var valid bool
-	valid = true
-	// fmt.Println(m1_len_r, m2_len_r, m1_len_c, m2_len_c)
-	if m1_len_r != m2_len_r || m1_len_c != m2_len_c {
-		valid = false
-	}
-	if valid {
-		for i:=0; i<m1_len_r; i++ {
-			for j:=0; j<m1_len_c; j++ {
-				res[i][j] = m1[i][j] - m2[i][j]
-			}
+func iterate(m1 [][]int, operation func(int, int)) {
+	for i, m1row := range m1 {
+		for j := range m1row {
+			operation(i, j)
 		}
 	}
-	return res, valid
 }
 
+// Add adds two matrices if their dimensions are the same
+// It returns a new matrix with the addition results,
+//   and the validity of the operation
+func Add(m1 [][]int, m2 [][]int) (res [][]int, valid bool) {
+	valid = isvalid(m1, m2)
+	if valid {
+		res = New(len(m1), len(m1[0]))
+
+		iterate(m1, func(i int, j int) {
+			res[i][j] = m1[i][j] + m2[i][j]
+		})
+	}
+
+	return
+}
+
+// Subtract subtracts two matrices, m2 from m1,  if their dimensions are the same
+// It returns a new matrix with the addition results,
+//   and the validity of the operation
+func Subtract(m1 [][]int, m2 [][]int) (res [][]int, valid bool) {
+	valid = isvalid(m1, m2)
+	if valid {
+		res = New(len(m1), len(m1[0]))
+
+		iterate(m1, func(i int, j int) {
+			res[i][j] = m1[i][j] - m2[i][j]
+		})
+	}
+
+	return
+}
+
+// String returns a string representation of the matrix
+func String(m [][]int) string {
+	result := ""
+
+	iterate(m, func(i int, j int) {
+		delim := "\t"
+		if j == 0 {
+			delim = "\n"
+		}
+
+		result = result + fmt.Sprintf("%s%4d", delim, m[i][j])
+
+	})
+
+	result += "\n"
+
+	return result
+}
+
+/*
 func Multiply(m1 [][]int, m2 [][]int) ([][]int, bool){
 	m1_len_r := len(m1)
 	m1_len_c := len(m1[0])
@@ -93,3 +121,4 @@ func Multiply(m1 [][]int, m2 [][]int) ([][]int, bool){
 	}
 	return res, valid
 }
+*/
